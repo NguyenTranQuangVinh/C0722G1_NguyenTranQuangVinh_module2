c63948d6aca6c209e86d7466169fd346a61cfc70
NguyenTranQuangVinh <ntqv22112004@gmail.com>
2022-10-07 20:10:28 +0700

    nop bai tap ss2

--- module2/src/ss2_loop_and_array/exercise/exercise10/TwoDimensionalArraySumTheNumbersAtTheMainDiagonalOfASquareMatrix.java
+++ module2/src/ss2_loop_and_array/exercise/exercise10/TwoDimensionalArraySumTheNumbersAtTheMainDiagonalOfASquareMatrix.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TwoDimensionalArraySumTheNumbersAtTheMainDiagonalOfASquareMatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n;
+
         int m;
         do {
             System.out.println("Nhập vào hàng: ");
@@ -14,19 +14,31 @@ public class TwoDimensionalArraySumTheNumbersAtTheMainDiagonalOfASquareMatrix {
                 System.out.println("Số lượng hàng > 0, xin nhập lại!!!");
             }
         } while (m <= 0);
-        do {
-            System.out.println("Nhập vào cột: ");
-            n = Integer.parseInt(scanner.nextLine());
-            if (n <= 0) {
-                System.out.println("Số lượng cột > 0, xin nhập lại!!!");
-            }
-        } while (n <= 0);
-        int[][] arr = new int[m][n];
-        for (int i = 0; i <m ; i++) {
-            for (int j = 0; j <n ; j++) {
-                System.out.println("Nhập vào phần tử giá trị thứ ["+i+"]["+j+"]: ");
-                arr[i][j]= Integer.parseInt(scanner.nextLine());
+
+        int[][] arr = new int[m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("Nhập vào phần tử giá trị thứ [" + i + "][" + j + "]: ");
+                arr[i][j] = Integer.parseInt(scanner.nextLine());
             }
         }
+
+        System.out.println("Ma trận vừa nhập là: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == j) {
+                    sum += arr[i][j];
+                }
+            }
+        }
+        System.out.println("Tổng các phần tử nằm trên đường chéo chính = " + sum);
     }
 }
--- module2/src/ss2_loop_and_array/exercise/exercise11/CountTheNumberOfOccurrencesOfACharacterInAString.java
+++ module2/src/ss2_loop_and_array/exercise/exercise11/CountTheNumberOfOccurrencesOfACharacterInAString.java
@@ -1,4 +1,22 @@
 package ss2_loop_and_array.exercise.exercise11;
 
+
+import java.util.Scanner;
+
 public class CountTheNumberOfOccurrencesOfACharacterInAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập chuỗi: ");
+        String str = scanner.nextLine();
+        System.out.print("Nhập ký tự:");
+        char characters = scanner.nextLine().charAt(0);
+
+        int count = 0;
+        for (int i = 0; i <str.length() ; i++) {
+          if(characters == str.charAt(i)){
+              count++;
+          }
+        }
+        System.out.println("Số kí tự đếm được là: "+count);
+    }
 }
