f97efe9e051d10dd3b99b9964c0474e032602406
NguyenTranQuangVinh <ntqv22112004@gmail.com>
2022-10-10 15:19:25 +0700

    nop bai tap ss3

---
+++ module2/src/ss3_class_and_object/exercise/exercise1/QuadraticEquation.java
@@ -0,0 +1,37 @@
+package ss3_class_and_object.exercise.exercise1;
+
+public class QuadraticEquation {
+    double a;
+    double b;
+    double c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        return this.b * this.b - 4 * this.a * this.c;
+    }
+    public double  getRoot1(){
+        double delta = getDiscriminant();
+        return (-this.b+Math.sqrt(getDiscriminant()))/(2*this.a) ;
+    }
+    public double getRoot2(){
+        return (-this.b-Math.sqrt(getDiscriminant()))/(2*this.a) ;
+    }
+}
+
---
+++ module2/src/ss3_class_and_object/exercise/exercise1/Test.java
@@ -0,0 +1,34 @@
+package ss3_class_and_object.exercise.exercise1;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập a: ");
+        double a = Double.parseDouble(scanner.nextLine());
+        System.out.println("Nhập b");
+        double b = Double.parseDouble(scanner.nextLine());
+        System.out.println("Nhập c");
+        double c = Double.parseDouble(scanner.nextLine());
+        QuadraticEquation equation = new QuadraticEquation(a, b, c);
+
+        if (equation.getA() == 0) {
+            System.out.println("Phương trình không phải là phương trình bậc hai. ");
+        } else {
+            double delta = equation.getDiscriminant();
+            if (delta >= 0) {
+                double root1 = equation.getRoot1();
+                double root2 = equation.getRoot2();
+                if (delta > 0) {
+                    System.out.println("Phương trình có 2 nghiệm: " + root1 + "and" + root2);
+                } else {
+                    System.out.println("Phương trình chỉ có 1 nghiệm: " + root1);
+                }
+
+            } else {
+                System.out.println("Phương trình không có nghiệm nguyên. ");
+            }
+        }
+    }
+}
