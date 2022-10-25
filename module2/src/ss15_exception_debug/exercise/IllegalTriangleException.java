3fe11edaa5b0fc8c5d07b0bc8055ed26417ca7e5
NguyenTranQuangVinh <ntqv22112004@gmail.com>
2022-10-25 20:39:07 +0700

    nop bai tap ss15

---
+++ module2/src/ss15_exception_debug/exercise/IllegalTriangleException.java
@@ -0,0 +1,8 @@
+package ss15_exception_debug.exercise;
+
+public class IllegalTriangleException extends Exception{
+    public IllegalTriangleException(String message){
+        super(message);
+    }
+
+}
---
+++ module2/src/ss15_exception_debug/exercise/Triangle.java
@@ -0,0 +1,64 @@
+package ss15_exception_debug.exercise;
+
+import java.util.Scanner;
+
+public class Triangle {
+    static double sideA;
+    static double sideB;
+    static double sideC;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Nhập cạnh A: ");
+                sideA = Double.parseDouble(scanner.nextLine());
+                checkSide(sideA);
+                break;
+            } catch (IllegalArgumentException | IllegalTriangleException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("cạnh sai định dangh");
+            }
+        }
+        while (true) {
+            try {
+                System.out.println("Nhập cạnh B: ");
+                sideB = Double.parseDouble(scanner.nextLine());
+                if (sideB < 0) {
+                    throw new IllegalArgumentException("Chiều dài cạnh sai");
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Cạnh sai định dạng");
+            }
+        }
+        try {
+            checkEdgeTriangle(sideC, sideB, sideA);
+        } catch (IllegalArgumentException | IllegalTriangleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void checkSide(double e) throws IllegalTriangleException {
+        if (e < 0) {
+            throw new IllegalTriangleException("Chiều dài cạnh sai");
+        }
+    }
+
+    static void checkEdgeTriangle(double c, double b, double a) throws IllegalTriangleException {
+        double sumAB = sideA + sideB;
+        double sumAC = sideA + sideC;
+        double sumBC = sideB + sideC;
+        if (c > sumAB || b > sumAC || a > sumBC) {
+            throw new IllegalTriangleException("Đây không phải 3 cạnh tam giác");
+        } else {
+            System.out.println("Đây là 3 cạnh tam giác");
+        }
+    }
+}
